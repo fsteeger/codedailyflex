5c040d7b28b6b626d73850f761a67eb6a52f46c5
Alex Alekseyenko <alex@alekseyenko.com>
2019-08-21 16:31:09 -0700

    Add "Bicep Curls" exercise

--- exercisesData.js
+++ exercisesData.js
@@ -22,6 +22,7 @@ export const data = {
     { name: "Lunge Walk", difficulty: "hard", gifName: "https://giphy.com/embed/SiCZG4nwYg9rjqI6px" },
     { name: "Calf Raises", difficulty: "easy", gifName: "https://giphy.com/embed/4VYaL0JOguZrBgjQOt" },
     { name: "Planks", difficulty: "medium", gifName: "https://giphy.com/embed/CLjw2mHysNEYw" },
+    { name: "Bicep Curls", difficulty: "easy", gifName: "https://giphy.com/gifs/fX86roUOhW6GTzksj5" },
     { name: "Burpees", difficulty: "hard", gifName: "https://giphy.com/embed/efe2FPcBhxxuiRPf80" }
   ],
   cooldowns: [
